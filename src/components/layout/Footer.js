a0167e8de63eebb37a51b56d42286f4bd0dfde58
Alan Jesus Carranza Vazquez <74394276+AlanJesusCV@users.noreply.github.com>
2026-03-16 15:37:24 -0600

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="footer">
-        Portafolo Alan Carranza Vazquez &copy; 
+        Portafolio Alan Carranza Vazquez &copy; 
     </footer>
   )
 }
